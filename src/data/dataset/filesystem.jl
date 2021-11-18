10951e836f3022bdeba618c83fe934572b6b9850
ferdiu <ferdiu.manzella@gmail.com>
2021-11-18 12:02:57 +0100

    Updated: loaddataset function to be more generic

--- src/data/dataset/filesystem.jl
+++ src/data/dataset/filesystem.jl
@@ -40,15 +40,15 @@ function _read_dataset_metadata(datasetdir::AbstractString)
     return dict
 end
 
-function _read_example_metadata(datasetdir::AbstractString, example_id::Integer)
+function _read_example_metadata(datasetdir::AbstractString, inst_id::Integer)
     @assert isfile(joinpath(
         datasetdir,
-        "$(_ds_inst_prefix)$(string(example_id))",
+        "$(_ds_inst_prefix)$(string(inst_id))",
         _ds_metadata
-    )) "Missing $(_ds_metadata) in dataset `$(_ds_inst_prefix)$(string(example_id))` in " *
+    )) "Missing $(_ds_metadata) in dataset `$(_ds_inst_prefix)$(string(inst_id))` in " *
         "`$(datasetdir)`"
 
-    file = open(joinpath(datasetdir, "$(_ds_inst_prefix)$(string(example_id))", _ds_metadata))
+    file = open(joinpath(datasetdir, "$(_ds_inst_prefix)$(string(inst_id))", _ds_metadata))
 
     dict = Dict{String,Any}()
     for (k, v) in split.(filter(x -> length(x) > 0, strip.(readlines(file))), '=')
@@ -81,7 +81,8 @@ end
 """
     datasetinfo(datasetpath)
 
-Show dataset size on disk and return the size in bytes.
+Show dataset size on disk and return a Touple with first element a Vector of selected IDs
+and second element the total size in bytes.
 """
 function datasetinfo(
     datasetpath::AbstractString;
@@ -130,6 +131,8 @@ function datasetinfo(
 
     totalsize = 0
 
+    # TODO: add exmple filtering by labels
+
     for id in examples_ids
         ex_metadata = _read_example_metadata(datasetpath, id)
         # TODO: perform some checks on metadata
@@ -147,35 +150,67 @@ function datasetinfo(
     return examples_ids, totalsize
 end
 
+function _load_instance(datasetpath::AbstractString, inst_id::Integer)
+    inst_metadata = _read_example_metadata(datasetpath, inst_id)
+
+    instancedir = joinpath(datasetpath, "$(_ds_inst_prefix)$(inst_id)")
+
+    frame_reg = Regex("$(_ds_frame_prefix)([[:digit:]]+).csv")
+    function isframefile(path::AbstractString)
+        return isfile(joinpath(instancedir, path)) && !isnothing(match(frame_reg, path))
+    end
+
+    frames = filter(isframefile, readdir(instancedir))
+    frames_num = sort!([match(frame_reg, f).captures[1] for f in frames])
+
+    result = Vector{Dict}(undef, length(frames_num))
+    #Threads.@threads
+    for (i, f) in collect(enumerate(frames_num))
+        frame_path = joinpath(instancedir, "$(_ds_frame_prefix)$(f).csv")
+        frame = CSV.File(frame_path) |> Tables.columntable |> pairs |> Dict
+        # TODO: use dim_frame_[[:digit:]] to properly load frames
+        result[i] = frame
+    end
+
+    return result
+end
+
 """
 TODO: docs
 """
-function loaddataset(datasetpath::AbstractString)
-    # TODO: add kwargs to load a subset of the dataset
-    @assert isdir(datasetpath) "Dataset at path $(datasetpath) does not exist"
+function loaddataset(
+    datasetpath::AbstractString;
+    onlywithlables::AbstractVector{Pair{String,Any}} = Pair{String,Any}[]
+)
+    selected_ids, datasetsize = datasetinfo(datasetpath, onlywithlables = onlywithlables)
 
-    # Get n_column from Metadata.txt
-    # TODO: generalize this 'magic number'
-    df = DataFrame([Vector{Float64}[] for i in 1:3], :auto)
+    @assert length(selected_ids) > 0 "No instance found"
 
-    # For each directory (in alphabetic order) in datasetpath
-    for i in sort(filter(i -> isdir(joinpath(datasetpath, i)), readdir(datasetpath, sort=true)), by = x -> parse(Int, chop(x, head=8, tail=0)))
-        # Go in every Example and search for Frame.csv
-        current_dir = joinpath(datasetpath, i)
-        for file in readdir(current_dir)
-            if startswith(file, "Frame")
-                # TODO: this can be done in a more efficent way
-                # Frame.csv to Dictionary
-                d = CSV.File(joinpath(current_dir, file)) |> Tables.columntable |> pairs |> Dict
-                # Push into DataFrame
-                # TODO: do not assume attribute names to be of type `:Integer`
-                push!(df, [d[Symbol(i)] for i in 0:(length(keys(d))-1)])
+    instance_frames = _load_instance(datasetpath, selected_ids[1])
+    frames_cols = [Symbol.(attr_name) for attr_name in keys.(instance_frames)]
+
+    df = DataFrame(
+        :ID => [selected_ids[1]],
+        [Symbol(k) => [v] for frame in instance_frames for (k, v) in frame]...
+    )
+
+    for id in selected_ids[2:end]
+        curr_row = Any[id]
+        for (i, frame) in enumerate(_load_instance(datasetpath, id))
+            for attr_name in frames_cols[i]
+                push!(curr_row, frame[attr_name])
             end
         end
+        push!(df, curr_row)
     end
 
-    return MultiFrameDataset([collect(1:nattributes(df))], df)
+    frame_descriptor = Vector{Integer}[]
+    df_names = Symbol.(names(df))
+    for (i, frame) in enumerate(frames_cols)
+        push!(frame_descriptor, [findfirst(x -> x == k, df_names) for k in frame])
+    end
 
+    return MultiFrameDataset(frame_descriptor, df)
 end
 
 """
