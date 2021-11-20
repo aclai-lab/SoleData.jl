645f5b3b56dcce08e5b7acd8282daad6480f7f91
ferdiu <ferdiu.manzella@gmail.com>
2021-11-20 17:49:53 +0100

    Generalized  (step 1)

--- src/data/dataset/describe.jl
+++ src/data/dataset/describe.jl
@@ -10,18 +10,30 @@ const desc_dict = Dict{Symbol,Function}(
     (getnames(catch22) .=> catch22)...
 )
 
+const auto_desc_by_dim = Dict{Integer,Vector{Symbol}}(
+    1 => [:mean_m, :min_m, :max_m]
+)
+
 function _describeonm(
     df::AbstractDataFrame;
     descfunction::Function,
     cols::AbstractVector{<:Integer} = 1:ncol(df),
-    t::AbstractVector{<:Tuple{Integer,Integer,Integer}},
-    kwargs...
+    t::AbstractVector{<:NTuple{3,Integer}} = [(1, 0, 0)]
 )
+    frame_dim = dimension(df)
+    @assert length(t) == 1 || length(t) == frame_dim "`t` length has to be `1` or the " *
+        "dimension of the frame ($(frame_dim))"
+
+    if frame_dim > 1 && length(t) == 1
+        # if dimension is > 1 but only 1 triple is passed use it for all dimensions
+        t = fill(t, frame_dim)
+    end
+
     x = Matrix{AbstractFloat}[]
 
     for j in cols
         y = Matrix{AbstractFloat}(undef, nrow(df), t[1][1])
-        for (i, paa_result) in enumerate(paa.(df[:,j]; f=descfunction ,decdigits=4, t=t))
+        for (i, paa_result) in enumerate(paa.(df[:,j]; f=descfunction, decdigits=4, t=t))
             y[i,:] = paa_result
         end
         push!(x, y)
@@ -36,46 +48,38 @@ end
 function describeonm(
     df::AbstractDataFrame;
     desc::AbstractVector{Symbol} = Symbol[],
-    t::AbstractVector{<:Tuple{Integer,Integer,Integer}},
-    kwargs...
+    t::AbstractVector{<:NTuple{3,Integer}} = [(1, 0, 0)],
 )
-
-    data = DataFrame()
-    data.variable = propertynames(df)
-
     for d in desc
-        data = insertcols!(data, ncol(data)+1, d => _describeonm(df; descfunction = desc_dict[d], t))
+        @assert d in keys(desc_dict) "`$(d)` is not a valid descriptor Symbol; available " *
+            "descriptors are $(keys(desc_dict))"
     end
 
-    return data
+    return DataFrame(
+        :Attirbutes => Symbol.(propertynames(df)),
+        [d => _describeonm(df; descfunction = desc_dict[d], t) for d in desc]...
+    )
 end
 
 # TODO: same as above
-function DF.describe(
-    mfd::AbstractMultiFrameDataset;
-    desc::AbstractVector{Symbol} = Symbol[],
-    t::AbstractVector{<:Tuple{Integer,Integer,Integer}} = [(1,0,0)],
-    kwargs...
-)
-    # TODO: make this assertions support Symbols from original `describe`
-    # for f in desc
-    #     @assert haskey(desc_dict, f) "Func not found"
-    # end
-
-    results = DataFrame[]
-
-    for frame in mfd
-        push!(results, describeonm(frame; desc, t, kwargs...))
-    end
-
-    return results
+function DF.describe(mfd::AbstractMultiFrameDataset; kwargs...)
+    return [DF.describe(mfd, i; kwargs...) for i in 1:nframes(mfd)]
 end
 
 # TODO: implement this
-# function DF.describe(mfd::MultiFrameDataset, stats::Union{Synbol, Pair}...; cols=:)
+# function DF.describe(mfd::MultiFrameDataset, stats::Union{Symbol, Pair}...; cols=:)
 #     # TODO: select proper defaults stats based on `dimension` of each frame
 # end
 
 function DF.describe(mfd::AbstractMultiFrameDataset, i::Integer; kwargs...)
-    DF.describe(frame(mfd, i), kwargs...)
+    frame_dim = dimension(frame(mfd, i))
+    if frame_dim == :mixed
+        # TODO: implement
+        throw(ErrorException("Description for `:mixed` dimension frame not implemented"))
+    elseif frame_dim == 0
+        return DF.describe(frame(mfd, i))
+    else
+        desc = haskey(kwargs, :desc) ? kwargs[:desc] : auto_desc_by_dim[frame_dim]
+        return describeonm(frame(mfd, i); desc = desc, kwargs...)
+    end
 end
