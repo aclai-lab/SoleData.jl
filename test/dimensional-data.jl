f73bfdcf4103fd441ba22893cb27c5b0afa211e1
giopaglia <24519853+giopaglia@users.noreply.github.com>
2023-11-29 17:47:19 +1100

    fix test

--- test/dimensional-data.jl
+++ test/dimensional-data.jl
@@ -20,14 +20,14 @@ for ((channel_size1, channel_size2), shouldfail) in [
 end
 
 
-df = dimensional2dataframe(eachslice(rand(3,4); dims=2), ["a", "b", "c"])
+df = SoleData.dimensional2dataframe(eachslice(rand(3,4); dims=2), ["a", "b", "c"])
 @test first(unique(size.(SoleData.dataframe2dimensional(df)[1]))) == (3,)
 
-df = dimensional2dataframe(eachslice(rand(1,3,4); dims=3), ["a", "b", "c"])
+df = SoleData.dimensional2dataframe(eachslice(rand(1,3,4); dims=3), ["a", "b", "c"])
 @test first(unique(size.(SoleData.dataframe2dimensional(df)[1]))) == (1,3)
 
-df = dimensional2dataframe(eachslice(rand(2,3,4); dims=3), ["a", "b", "c"])
+df = SoleData.dimensional2dataframe(eachslice(rand(2,3,4); dims=3), ["a", "b", "c"])
 @test first(unique(size.(SoleData.dataframe2dimensional(df)[1]))) == (2,3)
 
-df = dimensional2dataframe(eachslice(rand(2,2,3,4); dims=4), ["a", "b", "c"])
+df = SoleData.dimensional2dataframe(eachslice(rand(2,2,3,4); dims=4), ["a", "b", "c"])
 @test first(unique(size.(SoleData.dataframe2dimensional(df)[1]))) == (2,2,3)
