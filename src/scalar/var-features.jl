af31d3ecb354da2cc4e4e75860920248d27864af
giopaglia <24519853+giopaglia@users.noreply.github.com>
2024-08-22 14:35:27 +0200

    Improve doc

--- src/scalar/var-features.jl
+++ src/scalar/var-features.jl
@@ -183,12 +183,16 @@ end
     struct UnivariateFeature{U} <: AbstractUnivariateFeature
         i_variable::Integer
         f::Function
+        fname::Union{Nothing,String}
     end
 
 A dimensional feature represented by the application of a generic function `f`
 to a single variable of a dimensional channel.
 For example, it can wrap a scalar function computing
 how much red a `Interval2D` world, when interpreted on an image, contains.
+Optionally, a feature name `fname` can be attached to the function,
+which can be useful for inspection (e.g., if `f` is an anonymous function, this avoids
+names such s "#47" or "#49".
 
 See also [`SoleLogics.Interval`](@ref),
 [`SoleLogics.Interval2D`](@ref),
