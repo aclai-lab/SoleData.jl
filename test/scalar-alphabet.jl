4ca314031d2609e631effe1548ca93616ffe144e
giopaglia <24519853+giopaglia@users.noreply.github.com>
2024-11-06 15:56:26 +0100

    Fix test

--- test/scalar-alphabet.jl
+++ test/scalar-alphabet.jl
@@ -17,5 +17,5 @@ a = ExplicitAlphabet(collect(atoms(myalphabet_symbol)))
 @test_nowarn SoleData.scalaralphabet(a)
 
 a = ExplicitAlphabet(collect(atoms(alphabet(X, test_operators = [<, ≥])))
-@test_throws  SoleData.scalaralphabet(a; discretizedomain = true)
+@test_throws ErrorException SoleData.scalaralphabet(a; discretizedomain = true)
 @test_nowarn SoleData.scalaralphabet(a; discretizedomain = true, y = y)
