45756a66e7a60b32f9bbece141ce002b55669c79
PasoStudio73 <paso.studio73@gmail.com>
2025-04-01 16:32:43 +0200

    synthaxstring tested

--- test/var-features.jl
+++ test/var-features.jl
@@ -29,8 +29,10 @@ unf1 = UnivariateNamedFeature{U}(var_id, var_name)
 @test unf1 isa UnivariateNamedFeature{Float64, Int64}
 @test i_variable(unf1) == 1
 @test featurename(unf1) == "feature_name"
+@test syntaxstring(unf1) == "[feature_name]"
 
 unf2 = UnivariateNamedFeature(var_id, var_name)
 @test unf2 isa UnivariateNamedFeature{Real, Int64}
 @test i_variable(unf2) == 1
 @test featurename(unf2) == "feature_name"
+@test syntaxstring(unf2) == "[feature_name]"
